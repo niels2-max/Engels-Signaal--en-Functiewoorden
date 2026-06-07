d9668cf2a434f51f2129e89a87715b40be053b8f
Niels <niels2utb@gmail.com>
2026-06-07 16:17:07 +0200

    Update functiewoorden.js

--- data/functiewoorden.js
+++ data/functiewoorden.js
@@ -1,1 +1,22 @@
-
+const functiewoorden = [
+  {
+    english: "to clarify",
+    dutch: "duidelijk maken"
+  },
+  {
+    english: "to illustrate",
+    dutch: "illustreren"
+  },
+  {
+    english: "to emphasise",
+    dutch: "benadrukken"
+  },
+  {
+    english: "to contradict",
+    dutch: "tegenspreken"
+  },
+  {
+    english: "to summarise",
+    dutch: "samenvatten"
+  }
+];
