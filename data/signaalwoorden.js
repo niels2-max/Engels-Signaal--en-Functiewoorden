8c61854073638db777dba293c3d6136b386c42ff
Niels <niels2utb@gmail.com>
2026-06-07 16:16:54 +0200

    Update signaalwoorden.js

--- data/signaalwoorden.js
+++ data/signaalwoorden.js
@@ -1,1 +1,32 @@
-
+const signalwoorden = [
+  {
+    english: "however",
+    dutch: "echter",
+    category: "Tegenstelling"
+  },
+  {
+    english: "because",
+    dutch: "omdat",
+    category: "Reden / Oorzaak"
+  },
+  {
+    english: "therefore",
+    dutch: "daarom",
+    category: "Gevolg / Conclusie"
+  },
+  {
+    english: "although",
+    dutch: "hoewel",
+    category: "Tegenstelling"
+  },
+  {
+    english: "for example",
+    dutch: "bijvoorbeeld",
+    category: "Voorbeelden"
+  },
+  {
+    english: "if",
+    dutch: "als",
+    category: "Voorwaarde"
+  }
+];
